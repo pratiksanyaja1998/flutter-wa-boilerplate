b8692ba67150c754f51323f9af0443ce2f1625f2
ArchitRakholiya <104831197+ArchitRakholiya@users.noreply.github.com>
2022-11-22 15:54:00 +0530

    Initializing flutter project.

---
+++ whitelableapp/android/app/src/main/kotlin/com/example/whitelableapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.whitelableapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ whitelableapp/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Whitelable App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                blurRadius: 6
+              )
+            ],
+          ),
+          child: Text(
+            "Welcome to whitelable app",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+            ),
+          )
+        ),
+      ),
+    );
+  }
+}
